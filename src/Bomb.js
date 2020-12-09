870441c30ec2301f396a031e2a8f62d7d7cb0222
Jaquan1314 <javaughngrant@gmail.com>
2020-12-09 17:40:36 -0500

    commit tree 10d12a6d4bd497a8a124c485b78e29a938fbdf63

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
 // your Bomb code here!
+import React, { Fragment, Component } from 'react'
+
+class Bomb extends Component {
+  constructor(props) {
+    super()
+    // console.log(props.initialCount);
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    // console.log(this.props)
+    let message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+    return (
+      <Fragment>
+        {message}
+      </Fragment>
+    )
+  }
+}
+
+export default Bomb
