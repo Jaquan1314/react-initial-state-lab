7d0c252632f7553d14d54fc622c6def7f8da4043
Jaquan1314 <javaughngrant@gmail.com>
2020-12-09 17:40:51 -0500

    done

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
 // your ImageSlider code here!
+import React, { Fragment, Component } from 'react'
+
+class ImageSlider extends Component {
+  state = {
+    currentSlideIndex: 0
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <p>I am on slide { this.state.currentSlideIndex }</p>
+      </Fragment>
+    )
+  }
+}
+
+export default ImageSlider
